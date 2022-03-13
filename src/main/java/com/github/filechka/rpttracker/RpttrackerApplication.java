1068cc03ac6b5efe3ff498afbbac3bd50b9e5295
Kate Bobrova <ebobrova@lineate.com>
2022-03-13 15:50:52 +0600

    create spring template for web, lombok, jdbc, postgresq, h2

---
+++ src/main/java/com/github/filechka/rpttracker/RpttrackerApplication.java
@@ -0,0 +1,13 @@
+package com.github.filechka.rpttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpttrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpttrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/filechka/rpttracker/RpttrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.filechka.rpttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpttrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
